1330c82f6429e50d528b8082006887b7bae26cfc
John Doe <johndoe@email.com>
2023-06-13 22:28:38 +0600

    first commit

--- app/auth/router/router_update_my_account.py
+++ app/auth/router/router_update_my_account.py
@@ -1,7 +1,4 @@
-from typing import Any
-
-from fastapi import Depends
-from pydantic import Field
+from fastapi import Depends, Response
 
 from app.utils import AppModel
 
@@ -11,18 +8,18 @@ from . import router
 from .dependencies import parse_jwt_user_data
 
 
-class GetMyAccountResponse(AppModel):
-    id: Any = Field(alias="_id")
-    email: str
+class UpdateMyAccountRequest(AppModel):
     phone: str
     name: str
     city: str
 
 
-@router.get("/users/me", response_model=GetMyAccountResponse)
-def get_my_account(
+@router.patch("/users/me")
+def update_my_account(
+    input: UpdateMyAccountRequest,
     jwt_data: JWTData = Depends(parse_jwt_user_data),
     svc: Service = Depends(get_service),
 ) -> dict[str, str]:
-    user = svc.repository.get_user_by_id(jwt_data.user_id)
-    return user
+    svc.repository.update_user(jwt_data.user_id, input.dict())
+    # .dict() turns AppModel object into python dictionary
+    return Response(status_code=200)
